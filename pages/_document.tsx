b60368723c81cd56ed911da35d8658db4b87fa00
Prasad Nilkanth Kulkarni <kulkarniprasad57@gmail.com>
2023-08-13 18:45:14 +0530

    fonts link added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import Document, {
   DocumentContext,
   Head,
   Main,
-  NextScript
+  NextScript,
 } from 'next/document';
 
 export default class CustomDocument extends Document {
@@ -17,7 +17,22 @@ export default class CustomDocument extends Document {
   render() {
     return (
       <html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
